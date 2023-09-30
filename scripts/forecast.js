87012ba22aa87134b7368c85ab49adcc146d489f
Shanice Dawn <sdacleofewd@gmail.com>
2023-09-30 17:10:57 +0800

    updated code with Forecast Class

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,23 +1,27 @@
-const key = 'A9M0lWzP0mfSm5oltsuXIkhSdsUkpONM';
+class Forecast {
+    constructor() {
+        this.key = 'A9M0lWzP0mfSm5oltsuXIkhSdsUkpONM'
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/'
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    }
 
-// get weather information
-const getWeather = async (id) => {
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
-    const query = `${id}?apikey=${key}`;
+    async updateCity(city) {
+        const cityDets = await this.getCity(city)
+        const weather = await this.getWeather(cityDets.Key)
+        return { cityDets, weather }
+    }
 
-    const response = await fetch(base + query)
-    const data = await response.json()
+    async getCity(city) {
+        const query = `?apikey=${this.key}&q=${city}`
+        const response = await fetch(this.cityURI + query)
+        const data = await response.json()
+        return data[0];
+    }
 
-    return data[0];
-}
-
-// get city information
-const getCity = async (city) => {
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
-    const query = `?apikey=${key}&q=${city}`
-
-    const response = await fetch(base + query)
-    const data = await response.json()
-
-    return data[0];
+    async getWeather(id) {
+        const query = `${id}?apikey=${this.key}`;
+        const response = await fetch(this.weatherURI + query)
+        const data = await response.json()
+        return data[0];
+    }
 }
